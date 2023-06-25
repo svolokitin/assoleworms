9dcc1b4494089827f0d1302d1538b22d620c5720
MinecraftCompany <playin666@gmail.com>
2023-06-25 18:36:05 +0300

    beta1

--- MySQLdb.js
+++ MySQLdb.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql2');
 
+const sql = 'INSERT INTO members_band VALUE(?,?,?,?)';
+const member_band = [4, 1, 'Jorik_Shtuk', ''];
+
 const conn = mysql.createConnection({
 	host: "localhost",
 	user: "root",
@@ -7,6 +10,7 @@ const conn = mysql.createConnection({
 	password: "fynjyikzgbr35000!BOO"
 });
 
+//connect to data base
 conn.connect( function (err) {
 	if (err) {
 		return console.error("Не получилось, не фартануло " + err.message);
@@ -16,7 +20,19 @@ conn.connect( function (err) {
 	}
 });
 
+//added data to table
+conn.execute(sql, member_band, function (err, result) {
+	if (err) {
+		console.log(err)
+	}
+	else {
+		console.log('Member band added');
+	}
+});
+
+//show data from table
 conn.execute('SELECT * FROM members_band;', function (err, result) {
 	console.log(err);
 	console.log(result);
 });
+
