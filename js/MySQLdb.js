bc733a346f5d8b07b036eae74f982f3eb8fea7e6
svolokitin <volokitin14sv@gmail.com>
2023-06-29 21:34:12 +0300

    Hello from windows

--- js/MySQLdb.js
+++ js/MySQLdb.js
@@ -1,17 +1,19 @@
-const mysql = require('mysql2');
+import mysql2 from 'mysql2';
+import express from 'express';
 
 const sql = 'INSERT INTO members_band VALUE(?,?,?,?)';
 const member_band = [4, 1, 'Jorik_Shtuk', ''];
+const server = express();
 
-const conn = mysql.createConnection({
-	host: "localhost",
-	user: "root",
+const connection = mysql2.createConnection({
+	host: "185.250.205.64",
+	user: "DanteMachine_1",
 	database: "assoleworms",
 	password: "fynjyikzgbr35000!BOO"
 });
 
 //connect to data base
-conn.connect( function (err) {
+connection.connect( function (err) {
 	if (err) {
 		return console.error("Не получилось, не фартануло " + err.message);
 	}
@@ -21,17 +23,17 @@ conn.connect( function (err) {
 });
 
 //added data to table
-conn.execute(sql, member_band, function (err, result) {
+/*conn.execute(sql, member_band, function (err, result) {
 	if (err) {
 		console.log(err)
 	}
 	else {
 		console.log('Member band added');
 	}
-});
+});*/
 
 //show data from table
-conn.execute('SELECT * FROM members_band;', function (err, result) {
+connection.execute('SELECT * FROM members_band;', function (err, result) {
 	console.log(err);
 	console.log(result);
 });
